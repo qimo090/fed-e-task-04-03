fe2dbacd83cdfa5ff4504946b92e269a90419fae
qimo090 <grimm9563@gmail.com>
2021-02-21 02:26:30 +0800

    add: @emotion/react

---
+++ code/04-03-04/emotion-guide/src/App.js
@@ -0,0 +1,13 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+
+function App () {
+  return (
+    <div css={{ width: 200, height: 200, background: 'red' }}>
+      App works ...
+    </div>
+  )
+}
+
+export default App
