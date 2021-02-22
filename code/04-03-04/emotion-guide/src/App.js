22ba6416e01781e9efae467ada1b792ac33944ed
qimo090 <grimm9563@gmail.com>
2021-02-22 10:21:49 +0800

    emotion-guide init

--- code/04-03-04/emotion-guide/src/App.js
+++ code/04-03-04/emotion-guide/src/App.js
@@ -1,13 +1,25 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@emotion/react'
+import logo from './logo.svg';
+import './App.css';
 
-function App () {
+function App() {
   return (
-    <div css={{ width: 200, height: 200, background: 'red' }}>
-      App works ...
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
